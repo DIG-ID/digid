693e1b9cd4050cadc4bda6685ac464847d0aaf3f
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-01-11 20:51:16 +0000

    up

---
+++ template-parts/loops/loop-related-services.php
@@ -0,0 +1,11 @@
+<?php
+$related_services = get_field( 'related_services' );
+if ( $related_services ) :
+	$services_names = array();
+	foreach ( $related_services as $post ) :
+		setup_postdata( $post );
+		$services_names[] = get_the_title();
+	endforeach;
+	echo '<ul class="related-services"><li>' . implode( '<li>,</li>', $services_names ) . '</li></ul>';
+	wp_reset_postdata();
+endif;
--- template-parts/modules/cards/card-project.php
+++ template-parts/modules/cards/card-project.php
@@ -1,9 +1,5 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'card-project grid-item col-12 col-lg-6' ); ?>>
 	<img src="https://via.placeholder.com/529x343" alt="">
 	<a href="<?php the_permalink(); ?>" class="card-service__title"><?php the_title( '<h3>', '</h3> ' ); ?></a>
-	<?php
-	$term_obj_list = get_the_terms( $post->ID, 'taxonomy' );
-	$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name'));
-	echo $terms_string;
-	?>
+	<?php get_template_part( 'template-parts/loops/loop', 'related-services' ); ?>
 </article>
