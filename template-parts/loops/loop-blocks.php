0aff66bb0f4ec73c064c15992091c6eff55f9151
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-01-16 18:21:16 +0000

    up

--- template-parts/loops/loop-blocks.php
+++ template-parts/loops/loop-blocks.php
@@ -3,8 +3,7 @@
 	if ( have_rows( 'blocks' ) ) :
 		while ( have_rows( 'blocks' ) ) :
 			the_row();
-			$sub_value = get_sub_field( 'title' );
-			echo $sub_value;
+			get_template_part( 'template-parts/components/blocks/block', 'row' );
 		endwhile;
 	endif;
 	?>
