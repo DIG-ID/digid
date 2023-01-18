7a9c56bb5a7bbaae7f8810467b591aad4d3f0ab8
Daniel-Digid <daniel@dig.id>
2023-01-18 16:21:41 +0000

    Dev

---
+++ template-parts/pages/about/section-intro.php
@@ -0,0 +1,5 @@
+<section class="section section-about-intro">
+    <div class="container">
+        
+    </div>
+</section>
--- template-parts/pages/starting-project/section-form.php
+++ template-parts/pages/starting-project/section-form.php
@@ -1,12 +1,19 @@
 <section class="section section-st-project section-form">
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-between align-items-center">
             <div class="col-12 col-lg-7">
-                <p class="section__subtitle"><?php the_field('form_title') ?></p>
+                <p class="section__subtitle">
+                    <?php the_field('form_title') ?>
+                    <br>
+                    <?php echo esc_html__( 'Insert text', 'digid' ) ?>
+                </p>
             </div>
             <div class="col-12 col-lg-7">
                 <?php echo do_shortcode( get_post_meta( get_the_id(), 'form_shortcode', true ) ); ?>
             </div>
+            <div class="col-12 col-lg-4 bg-logo">
+				<?php get_template_part( 'template-parts/main-logo' ); ?>
+			</div>
         </div>
     </div>
 </section>
