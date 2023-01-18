e2bb93624516ac02f150999c8428cc4d0f5bd3f5
Daniel-Digid <daniel@dig.id>
2023-01-18 14:29:02 +0000

    Dev

--- template-parts/pages/starting-project/section-form.php
+++ template-parts/pages/starting-project/section-form.php
@@ -1,4 +1,4 @@
-<section class="section section-start-project section-form">
+<section class="section section-st-project section-form">
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-7">
