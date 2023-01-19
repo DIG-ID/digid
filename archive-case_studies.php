28e5a3bd01eba6408425acdcb47ec062f362a811
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-01-19 17:41:45 +0000

    up

--- archive-case_studies.php
+++ archive-case_studies.php
@@ -2,6 +2,6 @@
 get_header( 'dark' );
 	do_action( 'before_main_content' );
 	get_template_part( 'template-parts/archives/case-studies/section', 'intro' );
-	get_template_part( 'template-parts/modules/module', 'projects' );
+	get_template_part( 'template-parts/modules/module', 'latest-projects' );
 	do_action( 'after_main_content' );
 get_footer();
--- archive-services.php
+++ archive-services.php
@@ -3,6 +3,6 @@ get_header( 'light' );
 	do_action( 'before_main_content' );
 	get_template_part( 'template-parts/archives/services/section', 'intro' );
 	get_template_part( 'template-parts/archives/services/section', 'services-list' );
-	get_template_part( 'template-parts/modules/module', 'projects' );
+	get_template_part( 'template-parts/modules/module', 'latest-projects' );
 	do_action( 'after_main_content' );
 get_footer();
