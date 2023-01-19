1777d50c57849627e7b477bb09baed7ed85127b9
Daniel-Digid <daniel@dig.id>
2023-01-19 14:26:25 +0000

    Dev

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -3,7 +3,7 @@
  * Template Name: About Page Template
  */
 
-get_header();
+get_header('dark');
 	do_action( 'before_main_content' );
 	get_template_part( 'template-parts/pages/about/section', 'intro' );
 	do_action( 'after_main_content' );
