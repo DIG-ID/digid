c2a6d76bfd4696999a7d50c0fea79d27841c4756
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2023-01-24 16:37:08 +0000

    up

--- template-parts/posts/projects/project-navigation.php
+++ template-parts/posts/projects/project-navigation.php
@@ -1,26 +1,32 @@
-<section class="section-navigation">
-	<div class="container">
-		<div class="row">
-			<div class="col-6 previous-post">
-				<?php
-				$prev_post = get_previous_post();
-				if ( is_a( $prev_post , 'WP_Post' ) ) :
-					?>
-					<a href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>"><svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="32.4" height="11.9" style="enable-background:new 0 0 32.4 11.9"><path d="M32.4 5.3H2.6L6.9 1 6 0 0 6l6 6 1-1-4.4-4.4h29.8V5.3z" class="st0"/></svg> <?php echo esc_html( $prev_post->post_title ); ?></a>
+<?php
+$prev_post = get_previous_post();
+$next_post = get_next_post();
+
+if ( ! empty( $prev_post ) || ! empty( $next_post ) ) :
+	?>
+	<section class="section-navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-6 previous-post">
 					<?php
-				endif;
-				?>
-			</div>
-			<div class="col-6 text-end next-post">
-				<?php
-				$next_post = get_next_post();
-				if ( is_a( $next_post , 'WP_Post' ) ) :
+					if ( is_a( $prev_post , 'WP_Post' ) ) :
+						?>
+						<a href="<?php echo esc_url( get_permalink( $prev_post->ID ) ); ?>"><svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="32.4" height="11.9" style="enable-background:new 0 0 32.4 11.9"><path d="M32.4 5.3H2.6L6.9 1 6 0 0 6l6 6 1-1-4.4-4.4h29.8V5.3z" class="st0"/></svg> <?php echo esc_html( $prev_post->post_title ); ?></a>
+						<?php
+					endif;
 					?>
-					<a href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>"><?php echo esc_html( $next_post->post_title ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+				</div>
+				<div class="col-6 text-end next-post">
 					<?php
-				endif;
-				?>
+					if ( is_a( $next_post , 'WP_Post' ) ) :
+						?>
+						<a href="<?php echo esc_url( get_permalink( $next_post->ID ) ); ?>"><?php echo esc_html( $next_post->post_title ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+						<?php
+					endif;
+					?>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+	<?php
+endif;
