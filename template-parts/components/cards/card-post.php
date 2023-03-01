fac9e83535af3d42d48adbc0b2c224a20d6a9c38
Daniel-Digid <daniel@dig.id>
2023-03-01 15:18:43 +0000

    dev

--- template-parts/components/cards/card-post.php
+++ template-parts/components/cards/card-post.php
@@ -9,5 +9,5 @@
 	<a href="<?php the_permalink(); ?>" class="card-post__title"><?php the_title( '<h3>', '</h3> '); ?></a>
 	<?php get_template_part( 'template-parts/loops/loop', 'related-categories' ); ?>
 	<?php the_excerpt(); ?>
-	<a href="<?php the_permalink(); ?>" class="card-post__btn"><?php esc_html_e( 'Read more', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+	<a href="<?php the_permalink(); ?>" class="card-post__btn"><?php esc_html_e( 'Read more', 'digid' ); ?></a>
 </article>
