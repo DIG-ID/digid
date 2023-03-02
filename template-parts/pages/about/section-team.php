95abc7bf261d8cebd0208513f15c5815d7deb258
Daniel-Digid <daniel@dig.id>
2023-03-02 12:51:15 +0000

    dev

--- template-parts/pages/about/section-team.php
+++ template-parts/pages/about/section-team.php
@@ -1,8 +1,12 @@
 <section class="section section-about-team position-relative">
     <div class="container">
         <div class="row pb-5">
-            <div class="col-12 col-lg-4 offset-lg-1 p-0"><?php the_field('team_intro_left'); ?></div>
-            <div class="col-12 col-lg-4 offset-lg-2 p-0"><?php the_field('team_intro_right'); ?></div>
+            <div class="col-12 col-lg-5 offset-lg-1 p-0">
+                <p class="section__description-team"><?php the_field('team_intro_left'); ?></p>
+            </div>
+            <div class="col-12 col-lg-5 offset-lg-1 p-0">
+                <p class="section__description-team"><?php the_field('team_intro_right'); ?></p>
+            </div>
         </div>
         <div class="row align-items-center py-5">
             <div class="col-1">
@@ -14,6 +18,30 @@
         </div>
         <div class="row py-3">
             <h2 class="section__subtitle"><?php the_field('title_team'); ?></h2>
+        </div>
+    </div>
+    <div class="container px-4">
+        <div class="row">
+        <?php
+        if( have_rows('team_members_repeater') ):
+            while( have_rows('team_members_repeater') ) : the_row(); ?>
+            <div class="col-12 col-md-6 col-lg-3 px-0 mb-5">
+            <?php 
+            $image = get_sub_field('photo');
+            $size = 'full';
+            if( $image ) :
+                echo wp_get_attachment_image( $image, $size );
+            else:
+                echo '<figure><img src="https://via.placeholder.com/249x300" alt="placeholder image"></figure>';
+            endif; ?>
+            <p class="team__name"><?php the_sub_field('name'); ?></p>
+            <p class="team__position"><?php the_sub_field('position'); ?></p>
+            </div>
+        <?php 
+            endwhile;
+        endif; 
+        ?>
+            
         </div>
     </div>
 </section>
