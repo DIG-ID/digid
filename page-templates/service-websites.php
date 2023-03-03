1b7293a3080f6035e994df7407767a220027158b
Daniel-Digid <daniel@dig.id>
2023-03-03 14:49:36 +0000

    dev

--- page-templates/service-websites.php
+++ page-templates/service-websites.php
@@ -7,6 +7,7 @@
 get_header('light');
 	do_action( 'before_main_content' );
 	get_template_part( 'template-parts/archives/services/section', 'intro-child' );
+	get_template_part( 'template-parts/archives/services/websites/websites', 'blocks' );
 	get_template_part( 'template-parts/modules/module', 'call-to-action' );
 	get_template_part( 'template-parts/modules/module', 'related-projects' );
 	do_action( 'after_main_content' );
