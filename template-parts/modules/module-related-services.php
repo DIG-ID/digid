798f5af5a123daf7a3458bb7dd930402e1272723
Daniel-Digid <daniel@dig.id>
2023-03-06 15:09:33 +0000

    dev

--- template-parts/modules/module-related-services.php
+++ template-parts/modules/module-related-services.php
@@ -12,7 +12,6 @@
 			</div>
 		</div>
 		<div class="row">
-            
             <?php
             if ( is_singular( 'services' ) ) :
                 $pod = pods( 'services', get_the_id() );
@@ -27,14 +26,12 @@
                     <article id="post-<?php echo $rpod_id ?>" <?php post_class( 'col-12 col-lg-4 card-service' ); ?>>
                         <div class="card-service__content">
                             <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__title"><h3><?php echo get_the_title( $rpod_id ); ?></h3></a>
-                            <?php the_excerpt( $rpod_id ); ?>
+                            <p><?php echo get_the_excerpt( $rpod_id ); ?></p>
                             <hr>
                             <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__btn"><?php esc_html_e( 'Know more', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
                         </div>
                     </article>
-
                 <?php endforeach; ?>
-                
             <?php endif;
             ?>
 		</div>
