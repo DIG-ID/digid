7cb92507e8e9346d7c998a94249433531529164f
Daniel-Digid <daniel@dig.id>
2023-03-06 13:59:52 +0000

    dev

--- page-templates/service-newsletter.php
+++ page-templates/service-newsletter.php
@@ -13,6 +13,7 @@
     endif;
     get_template_part( 'template-parts/archives/services/newsletter-marketing/tiers' );
     get_template_part( 'template-parts/modules/module', 'call-to-action' );
+    get_template_part( 'template-parts/modules/module', 'related-services' );
 	get_template_part( 'template-parts/modules/module', 'related-projects' );
 	do_action( 'after_main_content' );
 get_footer();
---
+++ template-parts/modules/module-related-services.php
@@ -0,0 +1,45 @@
+<section class="section section-services">
+	<div class="container">
+	<div div class="row align-items-center section__title--wrapper">
+			<div class="col-1">
+				<span class="section__title--line"></span>
+			</div>
+			<div class="col-6">
+				<h2 class="section__title"><?php esc_html_e( 'Related services', 'digid' ); ?></h2>
+			</div>
+			<div class="col-5 text-end">
+				<a class="section__btn" href="<?php echo esc_url( get_post_type_archive_link( 'services' ) ); ?>"><?php esc_html_e( 'View all services', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+			</div>
+		</div>
+		<div class="row">
+            
+            <?php
+            if ( is_singular( 'services' ) ) :
+                $pod = pods( 'services', get_the_id() );
+            endif;
+            $rpods = $pod->field( 'related_services' );
+            if ( ! empty( $rpods ) ) :
+                $rnames = array();
+                foreach ( $rpods as $rpod ) :
+                    $rpod_id  = $rpod['ID'];
+                    $rnames[] = get_the_title( $rpod_id ); ?>
+                    
+                    <article id="post-<?php echo $rpod_id ?>" <?php post_class( 'col-12 col-lg-4 card-service' ); ?>>
+                        <div class="card-service__content">
+                            <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__title"><h3><?php echo get_the_title( $rpod_id ); ?></h3></a>
+                            <?php the_excerpt( $rpod_id ); ?>
+                            <hr>
+                            <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__btn"><?php esc_html_e( 'Know more', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+                        </div>
+                    </article>
+
+                <?php endforeach; ?>
+                
+            <?php endif;
+            ?>
+		</div>
+	</div>
+</section>
+
+
+
