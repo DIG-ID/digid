2d4b43a18c894eff4ed94d86c6d8f4f477f8e111
Daniel-Digid <daniel@dig.id>
2023-04-12 15:11:12 +0100

    dev

--- template-parts/modules/module-related-services.php
+++ template-parts/modules/module-related-services.php
@@ -24,12 +24,14 @@
                     $rnames[] = get_the_title( $rpod_id ); ?>
                     
                     <article id="post-<?php echo $rpod_id ?>" <?php post_class( 'col-12 col-lg-4 card-service' ); ?>>
+                    <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__content-link">
                         <div class="card-service__content">
-                            <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__title"><h3><?php echo get_the_title( $rpod_id ); ?></h3></a>
+                            <div class="card-service__title"><h3><?php echo get_the_title( $rpod_id ); ?></h3></div>
                             <p><?php echo get_the_excerpt( $rpod_id ); ?></p>
                             <hr>
-                            <a href="<?php the_permalink( $rpod_id ); ?>" class="card-service__btn"><?php esc_html_e( 'Know more', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+                            <div class="card-service__btn"><?php esc_html_e( 'Know more', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></div>
                         </div>
+                    </a>
                     </article>
                 <?php endforeach; ?>
             <?php endif;
