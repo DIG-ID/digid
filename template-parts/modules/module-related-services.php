7f74c9d2a4a1e0ab77d6aee4b42823da1abd9a59
Daniel-Digid <daniel@dig.id>
2023-04-17 17:07:46 +0100

    dev

--- template-parts/modules/module-related-services.php
+++ template-parts/modules/module-related-services.php
@@ -5,10 +5,10 @@
 				<span class="section__title--line"></span>
 			</div>
 			<div class="col-6">
-				<h2 class="section__title"><?php esc_html_e( 'Related services', 'digid' ); ?></h2>
+				<h2 class="section__title"><?php esc_html_e( 'Verwandte Dienstleistungen', 'digid' ); ?></h2>
 			</div>
 			<div class="col-5 text-end">
-				<a class="section__btn" href="<?php echo esc_url( get_post_type_archive_link( 'services' ) ); ?>"><?php esc_html_e( 'View all services', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
+				<a class="section__btn" href="<?php echo esc_url( get_post_type_archive_link( 'services' ) ); ?>"><?php esc_html_e( 'Zu allen Dienstleistungen', 'digid' ); ?> <svg xmlns="http://www.w3.org/2000/svg" width="32.439" height="11.914"><path d="M1 22.934h29.82l-4.305 4.306.967.967 5.957-5.957-5.957-5.957-.967.967 4.305 4.305H1Z" transform="translate(-1 -16.293)"/></svg></a>
 			</div>
 		</div>
 		<div class="row">
