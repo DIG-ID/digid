6a659c6b7525e99d8b7cb7e6d5ba814967a3f1f7
Daniel-Digid <daniel@dig.id>
2023-04-18 16:42:34 +0100

    dev

--- template-parts/pages/home/section-form.php
+++ template-parts/pages/home/section-form.php
@@ -1,10 +1,10 @@
 <section class="section section-form">
 	<div class="container">
 		<div div class="row align-items-center section__title--wrapper">
-			<div class="col-1">
+			<div class="col-3 col-md-1">
 				<span class="section__title--line"></span>
 			</div>
-			<div class="col-11">
+			<div class="col-9 col-md-11">
 				<h2 class="section__title"><?php echo esc_html_e( 'Kontaktieren Sie uns', 'digid' ); ?></h2>
 			</div>
 		</div>
