4bdc614eb68510bf143c9ce3711682e0c641d9bc
Daniel-Digid <daniel@dig.id>
2023-04-18 19:13:23 +0100

    dev

--- template-parts/pages/about/section-intro.php
+++ template-parts/pages/about/section-intro.php
@@ -9,10 +9,10 @@
 		</div>
 		<div class="container about__container">
 			<div class="row align-items-center section__title--wrapper">
-				<div class="col-1 d-none d-sm-block">
+				<div class="col-3 col-md-1">
 					<span class="section__title--line" style="background-color: #00CCCC;"></span>
 				</div>
-				<div class="col-12 col-lg-11">
+				<div class="col-9 col-md-11">
 					<h2 class="section__title">
 						<?php
 						if ( function_exists( 'yoast_breadcrumb' ) ) :
