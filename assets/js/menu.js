48df777903463949e70d4366e1a962b511aaaf16
Daniel-Digid <daniel@dig.id>
2023-04-21 15:13:07 +0100

    dev

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,3 +3,11 @@ document.querySelector('.hamburguer-button').addEventListener('click', function 
     document.querySelector('.animated-icon2').classList.toggle('open');
 });
 });
+
+$(function() {
+    $('.clickable').click(function() {
+        var index = $(this).data('index');
+        $('.services__overview-description').hide();
+        $('.services__overview-description:eq(' + (index - 1) + ')').show();
+    });
+    });
--- template-parts/archives/services/newsletter-marketing/services-overview.php
+++ template-parts/archives/services/newsletter-marketing/services-overview.php
@@ -5,23 +5,23 @@
 				<span class="section__title--line"></span>
 			</div>
 		</div>
+		
 		<div class="row">
-			<div class="col-12 col-lg-6">
+			<div class="col-12 col-lg-6 left-col">
 				<p class="services__overview-title"><?php the_field( 'service_overview_title' ); ?></p>
-				<p class="services__overview-description"><?php the_field( 'service_overview_description' ); ?></p>
+				<?php if ( have_rows( 'service_overview_service_descriptions' ) ) : ?>
+					<?php while ( have_rows( 'service_overview_service_descriptions' ) ) : the_row(); ?>
+						<p class="services__overview-description"><?php the_sub_field( 'description' ); ?></p>
+					<?php endwhile; ?>
+				<?php endif; ?>
 			</div>
-			<div class="col-12 col-lg-6">
-				<?php
-				if ( have_rows( 'service_overview_service_list' ) ) :
-					while ( have_rows( 'service_overview_service_list' ) ) :
-						the_row();
-						?>
-						<p class="services__overview-listItem"><?php the_sub_field( 'name' ); ?></p>
+			<div class="col-12 col-lg-6 right-col">
+				<?php if ( have_rows( 'service_overview_service_list' ) ) : ?>
+					<?php while ( have_rows( 'service_overview_service_list' ) ) : the_row(); ?>
+						<p class="services__overview-listItem clickable" data-index="<?php echo get_row_index(); ?>"><?php the_sub_field( 'name' ); ?></p>
 						<span class="section__title--line"></span>
-					<?php
-					endwhile;
-				endif;
-				?>
+					<?php endwhile; ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
