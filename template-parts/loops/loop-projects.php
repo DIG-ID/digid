80314cc979e46628b870bd24ad7bce5e9f0b6a6f
Daniel-Digid <daniel@dig.id>
2023-08-08 13:39:55 +0100

    dev

--- template-parts/loops/loop-projects.php
+++ template-parts/loops/loop-projects.php
@@ -4,6 +4,7 @@ $args = array(
 	'post_status' => 'publish',
 	'orderby'     => 'ID',
 	'order'       => 'DESC',
+	'posts_per_page' => -1
 );
 
 $the_query = new WP_Query( $args );
@@ -11,17 +12,15 @@ $the_query = new WP_Query( $args );
 if ( $the_query->have_posts() ) :
 	while ( $the_query->have_posts() ) :
 		$the_query->the_post();
-		switch ( $the_query->current_post ) :
-			case 1:
-				get_template_part( 'template-parts/components/cards/card', 'project-portrait' );
-				break;
-			case 2:
-				get_template_part( 'template-parts/components/cards/card', 'project-portrait' );
-				break;
-			default:
-				get_template_part( 'template-parts/components/cards/card', 'project-landscape' );
-				break;
-		endswitch;
+		// Determine which template to use based on the $alternate flag
+		if ( $alternate ) {
+			get_template_part( 'template-parts/components/cards/card', 'project-portrait' );
+		} else {
+			get_template_part( 'template-parts/components/cards/card', 'project-landscape' );
+		}
+
+		// Toggle the $alternate flag for the next iteration
+		$alternate = ! $alternate;
 	endwhile;
 	wp_reset_postdata();
 endif;
