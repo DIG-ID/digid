20141c9419a0efec565f2e1deab4be98b8d2c0dc
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-02-19 11:49:33 +0000

    up

--- template-parts/posts/projects/project-featured-image.php
+++ template-parts/posts/projects/project-featured-image.php
@@ -5,7 +5,7 @@
 				<?php
 				if ( has_post_thumbnail() ) :
 					the_post_thumbnail( 'single-project-featured-image' );
-				else:
+				else :
 					echo '<figure><img src="https://via.placeholder.com/1080x720" alt="placeholder image"></figure>';
 				endif;
 				?>
