29fdc887ac45b53cebec09fba4696c9750749ea9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-04-15 17:37:44 +0100

    up

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -8,7 +8,7 @@ get_header( 'dark' );
 		get_template_part( 'template-parts/pages/about/section', 'intro' );
 		//get_template_part( 'template-parts/pages/about/section', 'team' );
 		get_template_part( 'template-parts/pages/about/section', 'team-full-time' );
-		get_template_part( 'template-parts/pages/about/section', 'team-freelancers' );
+		//get_template_part( 'template-parts/pages/about/section', 'team-freelancers' );
 		get_template_part( 'template-parts/pages/about/section', 'contact' );
 		//get_template_part( 'template-parts/pages/about/section', 'blog' );
 		get_template_part( 'template-parts/pages/about/section', 'partners' );
