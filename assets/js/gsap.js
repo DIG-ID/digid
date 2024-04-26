3d4bb28a809a2bc951f4c663ab160ed5acdef25e
Daniel-Digid <daniel@dig.id>
2024-04-26 13:16:24 +0100

    d

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -119,14 +119,14 @@ $(function() {
          trigger: ".section-projects",
          //start: "-100px 90%",
          start: "top 100%",
-         end: "bottom 100%",
+         end: "bottom 130%",
          scrub: 1,
          //markers: true,
          toggleActions: "restart pause reverse pause"
        },
      });
      archiveProjectsTl
-       .from(".card-project", { autoAlpha: 0, y:'50px', opacity: 0, duration: 0.5, stagger: '0.20', ease: "power3.out" });
+       .from(".card-project", { autoAlpha: 0, y:'10px', opacity: 0, duration: 0.5, stagger: '0.20', ease: "power3.out" });
    }
 
   /*if ( $(".single-services")[0] ) {
