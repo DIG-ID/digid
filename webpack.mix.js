ce5105b674bc2cfe254dbb72e32fa75853769ec8
Pedro <pedro@dig.id>
2026-03-11 11:13:48 +0000

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,16 @@
 // webpack.mix.js
 
+const os = require('os');
+const path = require('path');
+
+// Caminho para os certificados do Local by WPEngine
+const certPath = path.join(
+  os.homedir(),
+  os.platform() === 'win32'
+    ? 'AppData/Roaming/Local/run/router/nginx/certs'
+    : 'Library/Application Support/Local/run/router/nginx/certs'
+);
+
 let mix = require('laravel-mix');
 
 mix
@@ -13,9 +24,14 @@ mix
   .sass('assets/sass/admin-login.sass', 'css')
   .disableNotifications()
   .browserSync({
-    proxy: {
-      target: "http://digid.digid",
-      ws: true
+    proxy: "https://digid.digid",
+    host: "digid.digid",
+    open: "external",
+    port: 3000,
+    ws: true,
+    https: {
+      key: path.join(certPath, 'digid.digid.key'),
+      cert: path.join(certPath, 'digid.digid.crt'),
     },
     files: ["./**/*.php", "./dist/*.js", "./dist/*.css"]
   });
